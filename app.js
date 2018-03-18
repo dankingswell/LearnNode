6cdc26c45883aa87d9b05a9602a73a1a2a15963a
Dan Kingswell <dankingswell@dans-mbp.home>
2018-03-18 15:18:21 +0000

    testing compression unzip

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
 var hello = require("./Hello.js");
 var fs = require("fs");
+var zlib = require("zlib");
 
-var Reader = fs.createReadStream("./DataToPipe")
-var Writer = fs.createWriteStream("./FileToBePiped")
+
+
+var Reader = fs.createReadStream(__dirname + "/DataToPipe.txt");
+var Writer = fs.createWriteStream(__dirname + "/FileToBePiped.txt");
+
+var Zipstream = zlib.createGzip();
+var Compressed = fs.createWriteStream(__dirname +  "/CompressedPipe.gz");
+var UnzippedGunzip =zlib.createGunzip();
+var Unzipped =fs.createWriteStream(__dirname + "/Uncompressed.txt");
+
 
 Reader.pipe(Writer);
+Reader.pipe(Zipstream).pipe(Compressed);
+Zipstream.pipe(UnzippedGunzip).pipe(Unzipped);
+//console.log(Unzipped)
 
 hello();
