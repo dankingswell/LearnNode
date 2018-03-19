353f4fb30437602f53c99241e3cba594897d500f
Dan Kingswell <dankingswell@dans-mbp.home>
2018-03-19 11:35:55 +0000

    added routing to server for 404

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,8 @@ var fs =  require("fs");
 
 http.createServer(function(req, res)
 {
+    if (req.url === "/Json")
+    {
     res.writeHead(200,
          { "Content-Type" :"application/Json" }
         );
@@ -14,6 +16,14 @@ http.createServer(function(req, res)
             Last:"King"
         };
         res.end(JSON.stringify(obj));
+    }
+    else
+    {
+        
+        res.writeHead(404,{"Content_type":"Text/plain"});
+        res.end("No page to be found here.");
+        
 
+    }
 }).listen(1300,"127.0.0.1");
 
